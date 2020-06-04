91285c62391d333a3cacbfb626e4f38610de9f3a
Rafael Delboni <rafadelboni@gmail.com>
2020-06-03 22:43:56 -0300

    feat: draw fn Sketch

--- src/graphmosphere/core.clj
+++ src/graphmosphere/core.clj
@@ -6,11 +6,12 @@
 
 (defn -main [& args]
   (q/sketch
-  :title "You spin my circle right round"
-  :renderer :p3d
-  :size [500 500]
-  :setup #(draw/setup-state {:a "a"})
-  :draw #'draw/draw-state
-  :on-close #'draw/on-close
-  :features [:keep-on-top]
-  :middleware [m/fun-mode]))
+    :title "graphmosphere"
+    :renderer :p3d
+    :size [500 500]
+    :setup #(draw/setup-state {:a "a"})
+    :update #'draw/update-state
+    :draw #'draw/draw-state
+    :on-close #'draw/on-close
+    :features [:keep-on-top]
+    :middleware [m/fun-mode]))
--- src/graphmosphere/draw.clj
+++ src/graphmosphere/draw.clj
@@ -1,14 +1,40 @@
 (ns graphmosphere.draw
   (:require [quil.core :as q]))
 
-(defn setup-state [state]
-  (merge {:b "b"} state))
+(def test-temporary-state
+  {:shapes
+   [{:type   :sphere
+     :radius 25
+     :pos    {:x 50 :y 10 :z 0}
+     :echo   {:iterations 1 :variation 10}}
+    {:type   :sphere
+     :radius 25
+     :pos    {:x 10 :y 100 :z 0}
+     :echo   {:iterations 1 :variation 10}}]})
 
-(defn draw-state [_state]
+(defn setup-state [state]
+  (merge {} state))
+
+(defn update-state [_state]
+  test-temporary-state)
+
+(defn draw-sphere
+  [{:keys [radius] {:keys [x y z]} :pos}]
+  (q/translate x y z)
+  (q/sphere radius)
+  (q/translate (- x) (- y) (- z)))
+
+(defn draw-shape
+  [{:keys [type] :as shape}]
+  (case type
+    :sphere (draw-sphere shape)
+    (q/debug shape)))
+
+(defn draw-state [state]
   (q/clear)
-  (q/camera 150 150 150 0 0 0 0 0 1)
-  (q/shininess 10)
-  (q/sphere 50)
+  (->> (:shapes state)
+       (run! #(draw-shape %)))
+  (q/camera 10 150 150 0 0 0 0 0 1)
   (q/rect 0 0 100 100)
   (q/translate 0 0 100)
   (q/box 70))
