5cb4caf3be432cc09dc976df7dda3ffa0c0b4bf5
Rafael Delboni <rafadelboni@gmail.com>
2020-06-17 17:34:41 -0300

    feat: Adds triangle and rect

--- src/graphmosphere/draw.clj
+++ src/graphmosphere/draw.clj
@@ -41,7 +41,24 @@
     {:type   :ellipse
      :shape  {:width 5 :height 15}
      :draw   {:fill   {:r 255 :g 255 :b 255 :a 255}
-              :pos    {:x 50 :y -50 :z 0 :angle q/QUARTER-PI}}}]})
+              :pos    {:x 50 :y -50 :z 0 :angle q/QUARTER-PI}}}
+    {:type   :line
+     :shape  {:x1 0 :y1 0 :z1 0 :x2 10 :y2 10 :z2 0}
+     :draw   {:stroke {:r 0 :g 0 :b 255 :a 255}
+              :pos    {:x -50 :y 25 :z 0}}}
+    {:type   :point
+     :shape  {:x 0 :y 0 :z 0}
+     :draw   {:stroke {:r 255 :g 255 :b 255 :a 255 :weight 5}
+              :pos    {:x -50 :y -50 :z 0}}}
+    {:type   :triangle
+     :shape  {:x1 0 :y1 0 :x2 5 :y2 10 :x3 10 :y3 0}
+     :draw   {:stroke {:r 255 :g 255 :b 255 :a 255}
+              :pos    {:x 0 :y -50 :z 0}}}
+    {:type   :rect
+     :shape  {:x 0 :y 0 :width 10 :height 5 :top-left-r 0 :top-right-r 0 :bottom-right-r 0 :bottom-left-r 0}
+     :draw   {:stroke {:r 255 :g 255 :b 255 :a 255}
+              :fill   {:r 125 :g 125 :b 125 :a 255}
+              :pos    {:x 100 :y -100 :z 0}}}]})
 
 ; pure logic fns
 (defn calculate-echo-variation
@@ -118,9 +135,26 @@
   [{:keys [draw] {:keys [width height start stop mode]} :shape}]
   (draw-func-with-args #(q/arc 0 0 width height start stop mode) draw))
 
-(defn draw-2d-ellipse 
+(defn draw-2d-ellipse
   [{:keys [draw] {:keys [width height]} :shape}]
   (draw-func-with-args #(q/ellipse 0 0 width height) draw))
+
+(defn draw-2d-line
+  [{:keys [draw] {:keys [x1 y1 z1 x2 y2 z2]} :shape}]
+  (draw-func-with-args #(q/line x1 y1 z1 x2 y2 z2) draw))
+
+(defn draw-2d-point
+  [{:keys [draw] {:keys [x y z]} :shape}]
+  (draw-func-with-args #(q/point x y z) draw))
+
+(defn draw-2d-triangle
+  [{:keys [draw] {:keys [x1 y1 x2 y2 x3 y3]} :shape}]
+  (draw-func-with-args #(q/triangle x1 y1 x2 y2 x3 y3) draw))
+
+(defn draw-2d-rect
+  [{:keys [draw]
+    {:keys [x y width height top-left-r top-right-r bottom-right-r bottom-left-r]} :shape}]
+  (draw-func-with-args #(q/rect x y width height top-left-r top-right-r bottom-right-r bottom-left-r) draw))
 
 (defn draw-shape
   [{:keys [type] :as shape}]
@@ -130,6 +164,10 @@
     :quad (draw-2d-quad shape)
     :arc (draw-2d-arc shape)
     :ellipse (draw-2d-ellipse shape)
+    :line (draw-2d-line shape)
+    :point (draw-2d-point shape)
+    :triangle (draw-2d-triangle shape)
+    :rect (draw-2d-rect shape)
     (q/debug shape)))
 
 (defn draw-state [state]
